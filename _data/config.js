75572ffff5e2d94858f45caf29df04c71374ac86
Yonnji <yonnji@kitsune.one>
2026-06-03 10:53:47 +0300

    separate website configs

---
+++ _data/config.js
@@ -0,0 +1,10 @@
+export default function() {
+    const build = process.env.ELEVENTY_RUN_MODE === 'build';
+
+    const config = {
+        index: 'index.html',
+        api: build ? 'https://den.kitsune.one/api/' : 'http://localhost:8000/api/',
+        use_altcha: true,
+    }
+    return config;
+}
