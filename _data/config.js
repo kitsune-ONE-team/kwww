2ccd67b6457d2c037de0e03c082eea3d7350af7b
Yonnji <yonnji@kitsune.one>
2026-06-06 00:05:01 +0300

    add dashboard

--- _data/config.js
+++ _data/config.js
@@ -2,9 +2,19 @@ export default function() {
     const build = process.env.ELEVENTY_RUN_MODE === 'build';
 
     const config = {
-        index: 'index.html',
+        // index: 'index.html',
         api: build ? 'https://den.kitsune.one/api' : 'http://localhost:8000/api',
         use_altcha: true,
+
+        // https://itch.io/docs/api/oauth
+        itch_url: 'https://itch.io/user/oauth',
+        itch_params: {
+            client_id: '76ff64840aab284b65813c9ce2e08a13',
+            scope: 'profile:me',
+            response_type: 'token',
+            // redirect_uri: 'https://kitsune.one/login/itch/index.html',
+            redirect_uri: 'https://kitsune.one/login/itch/',
+        },
     }
     return config;
 }
